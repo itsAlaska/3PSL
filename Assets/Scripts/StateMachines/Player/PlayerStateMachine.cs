007afbac68a4693e92d3b12a7a3cded501707315
Scott Mackey <scott.thomas.mackey@gmail.com>
2023-05-25 00:28:09 -0400

    Add character controller
    Also add in some logic so that the read value from the input reader
    moves the player in an expected way. Also the rotation of the player
    prefab now turns to face the direction they are moving.

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -5,6 +5,8 @@ namespace StateMachines.Player
     public class PlayerStateMachine : StateMachine
     {
         [field: SerializeField] public InputReader InputReader { get; private set; }
+        [field: SerializeField] public CharacterController Controller { get; private set; }
+        [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
         
         void Start()
         {
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -15,13 +15,17 @@ namespace StateMachines.Player
         public override void Tick(float deltaTime)
         {
             Vector2 movementValue = StateMachine.InputReader.MovementValue;
-            Vector3 movement = new Vector3
-            {
-                x = movementValue.x,
-                y = 0,
-                z = movementValue.y
-            };
-            StateMachine.transform.Translate(movement * deltaTime);
+            Vector3 movement = new Vector3();
+
+            movement.x = movementValue.x;
+            movement.y = 0;
+            movement.z = movementValue.y;
+
+            StateMachine.Controller.Move(movement * StateMachine.FreeLookMovementSpeed * deltaTime);
+
+            if (StateMachine.InputReader.MovementValue == Vector2.zero) return;
+            
+            StateMachine.transform.rotation = Quaternion.LookRotation(movement);
         }
 
         public override void Exit()
