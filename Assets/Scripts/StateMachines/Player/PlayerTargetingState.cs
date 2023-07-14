73054eda78184f0a062fe8c1fa49b9ab1b48de4e
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-07-13 23:29:39 -0400

    Adds targeting state animations
    Now the character has animations specifically for when they are in the
    targeting state.

--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -1,11 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations;
 
 namespace StateMachines.Player
 {
     public class PlayerTargetingState : PlayerBaseState
     {
         private readonly int _targetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+        private readonly int _targetingForwardHash = Animator.StringToHash("TargetingForwardSpeed");
+        private readonly int _targetingRightHash = Animator.StringToHash("TargetingRightSpeed");
+
+        private const float AnimatorDampTime = 0.1f;
 
         public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine)
         {
@@ -31,8 +36,11 @@ namespace StateMachines.Player
             Move(movement * (StateMachine.TargetingMovementSpeed * StateMachine.InputReader.MovementValue.magnitude),
                 deltaTime);
 
+            UpdateAnimator(deltaTime);
+
             FaceTarget();
         }
+
 
         public override void Exit()
         {
@@ -60,6 +68,60 @@ namespace StateMachines.Player
             movement += transform.forward * StateMachine.InputReader.MovementValue.y;
 
             return movement;
+        }
+
+        private void UpdateAnimator(float deltaTime)
+        {
+            var animator = StateMachine.Animator;
+            var movementValue = StateMachine.InputReader.MovementValue;
+            var currentForwardValue = animator.GetFloat(_targetingForwardHash);
+            var currentRightValue = animator.GetFloat(_targetingForwardHash);
+            const float threshold = 0.001f;
+            Debug.Log($"movementValue.y: {movementValue.y}");
+            Debug.Log($"movementValue.x: {movementValue.x}");
+
+            if (movementValue.y == 0)
+            {
+                if (Mathf.Abs(currentForwardValue) < threshold) animator.SetFloat(_targetingForwardHash, 0);
+                else animator.SetFloat(_targetingForwardHash, 0, AnimatorDampTime, deltaTime);
+            }
+            else
+            {
+                animator.SetFloat(_targetingForwardHash, movementValue.y, AnimatorDampTime, deltaTime);
+            }
+
+            if (movementValue.x == 0)
+            {
+                if (Mathf.Abs(currentRightValue) < threshold) animator.SetFloat(_targetingRightHash, 0);
+                else animator.SetFloat(_targetingRightHash, 0, AnimatorDampTime, deltaTime);
+            }
+            else
+            {
+                animator.SetFloat(_targetingRightHash, movementValue.x, AnimatorDampTime, deltaTime);
+            }
+
+            // if (movementValue == Vector2.zero)
+            // {
+            //     var currentForwardValue = animator.GetFloat(_targetingForwardHash);
+            //     var currentRightValue = animator.GetFloat(_targetingRightHash);
+            //     const float threshold = 0.001f;
+            //
+            //     if (Mathf.Abs(currentForwardValue) < threshold && Mathf.Abs(currentRightValue) < threshold)
+            //     {
+            //         animator.SetFloat(_targetingForwardHash, 0);
+            //         animator.SetFloat(_targetingRightHash, 0);
+            //     }
+            //     else
+            //     {
+            //         animator.SetFloat(_targetingForwardHash, 0, AnimatorDampTime, deltaTime);
+            //         animator.SetFloat(_targetingRightHash, 0, AnimatorDampTime, deltaTime);
+            //     }
+            // }
+            // else
+            // {
+            //     animator.SetFloat(_targetingForwardHash, movementValue.y, AnimatorDampTime, deltaTime);
+            //     animator.SetFloat(_targetingRightHash, movementValue.x, AnimatorDampTime, deltaTime);
+            // }
         }
     }
 }
